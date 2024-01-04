477a037e4a20f50d688b2ec45736b12f872b02ba
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-04 11:17:00 +0100

    reads backend

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+const backendUrl = 'http://localhost:5123';
 
 export const PageWelcome = () => {
-	const [appName, setAppName] = useState('nnn');
+	const [appName, setAppName] = useState('');
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(backendUrl);
+			setAppName(response.data.appName)
+		})();
+	}, []);
 
 	return (
 		<p>APPNAME: {appName}</p>
