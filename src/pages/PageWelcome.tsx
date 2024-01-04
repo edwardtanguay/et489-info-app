81d4259d6c9e849fe2ed92a0451c4b8e392bcbc1
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-04 11:23:01 +0100

    shows frameworks

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,19 +1,43 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const backendUrl = 'http://localhost:5123';
+const backendUrl = "http://localhost:5123";
+
+interface IFramework {
+	name: string;
+	url: string;
+	description: string;
+}
 
 export const PageWelcome = () => {
-	const [appName, setAppName] = useState('');
+	const [appName, setAppName] = useState("");
+	const [frameworks, setFrameworks] = useState<IFramework[]>([]);
 
 	useEffect(() => {
 		(async () => {
 			const response = await axios.get(backendUrl);
-			setAppName(response.data.appName)
+			setAppName(response.data.appName);
+		})();
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}/frameworks`);
+			setFrameworks(response.data);
 		})();
 	}, []);
 
 	return (
-		<p>APPNAME: {appName}</p>
-	)
-}
+		<>
+			<p>APPNAME: {appName}</p>
+			<div>
+				FRAMEWORKS:
+				<ul className="list-disc ml-6">
+					{frameworks.map((framework, index) => {
+						return <li key={index}><a href={framework.url} target="_blank" className="underline font-semibold">{framework.name}</a> - {framework.description}</li>;
+					})}
+				</ul>
+			</div>
+		</>
+	);
+};
